3bbcc512610cde0548b2cdff29761d673e8c135a
Dirga Yasa <cekingx@gmail.com>
2021-07-08 12:20:27 +0800

    download openethereum

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 apt update -y
+apt upgrade -y
+
+apt install unzip -y
+wget https://github.com/openethereum/openethereum/releases/download/v3.2.6/openethereum-linux-v3.2.6.zip
+unzip openethereum-linux-v3.2.6.zip
