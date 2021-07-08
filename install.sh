55832e7990a6a37d9c94a7d6e1fead931462a9b9
Dirga Yasa <cekingx@gmail.com>
2021-07-08 12:33:41 +0800

    change file permission

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ apt upgrade -y
 apt install unzip -y
 wget https://github.com/openethereum/openethereum/releases/download/v3.2.6/openethereum-linux-v3.2.6.zip
 unzip openethereum-linux-v3.2.6.zip
+
+chmod +x ethkey ethstore openethereum openethereum-evm
