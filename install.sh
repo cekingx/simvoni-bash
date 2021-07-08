0ce82a2dfd8189711bc6f26a97cf70dd3dbc98c7
Dirga Yasa <cekingx@gmail.com>
2021-07-08 17:29:00 +0800

    remove directory

--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@ apt update -y
 apt upgrade -y
 
 apt install unzip -y
-mkdir openethereum
-cd openethereum
 wget https://github.com/openethereum/openethereum/releases/download/v3.2.6/openethereum-linux-v3.2.6.zip
 unzip openethereum-linux-v3.2.6.zip
 
 chmod +x ethkey ethstore openethereum openethereum-evm
-echo 'export PATH="$HOME/openethereum:$PATH"' >> ~/.bashrc
-source ~/.bashrc
