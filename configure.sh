a3346b1c9ce19513bf83f90bcf107c800036e96c
Dirga Yasa <cekingx@gmail.com>
2021-07-08 16:01:56 +0800

    add to path

---
+++ configure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd $HOME
+openethereum --version
--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ apt update -y
 apt upgrade -y
 
 apt install unzip -y
+mkdir openethereum
+cd openethereum
 wget https://github.com/openethereum/openethereum/releases/download/v3.2.6/openethereum-linux-v3.2.6.zip
 unzip openethereum-linux-v3.2.6.zip
 
 chmod +x ethkey ethstore openethereum openethereum-evm
+export PATH="$HOME/openethereum:$PATH"
+source ~/.bashrc
